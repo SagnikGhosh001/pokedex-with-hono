3940a69ea9f64801ba296c98e1c26e7a53747dbb
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2026-03-01 23:01:22 +0530

    complete all css

--- src/handler/request_handler.js
+++ src/handler/request_handler.js
@@ -1,18 +1,30 @@
 import { Eta } from "eta";
 
-const filterPoekmon = (pokemons, type) =>
-  pokemons.filter((pokemon) =>
-    pokemon.types.some((t) => t.type.name === type) || type === "all"
-  );
+const filterPoekmon = (pokemons, searchedPokemon, type) =>
+  pokemons.filter((pokemon) => {
+    const matchesType = type === "all" ||
+      pokemon.types.some((t) => t.type.name === type);
+
+    const matchesSearch = !searchedPokemon ||
+      pokemon.name.toLowerCase().includes(searchedPokemon);
+
+    return matchesType && matchesSearch;
+  });
 
 export const displayPokemons = (c) => {
   const pokemons = c.get("pokemons");
   const type = c.req.param("type") || "all";
+  const pokemon = c.req.query("pokemon")?.toLowerCase() || "";
+  const path = new URL(c.req.url).pathname;
+
   const eta = new Eta({ views: "./public/templates" });
-  const filteredPokemon = filterPoekmon(pokemons, type);
+  const filteredPokemon = filterPoekmon(pokemons, pokemon, type);
   const page = eta.render("pokemons.html", {
     pokemons: filteredPokemon,
     selectedType: type,
+    searchQuery: pokemon,
+    path,
   });
+
   return c.html(page);
 };
